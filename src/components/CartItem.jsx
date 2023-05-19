280b9ff48dc5883942892700655e647769ded488
sujitmemane <officialsujitmemane@gmail.com>
2023-05-19 13:14:11 +0530

    completed-cart

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,11 +1,22 @@
 import React from "react";
-
+import { AiFillPlusCircle, AiFillMinusCircle } from "react-icons/ai";
+import { ImCross } from "react-icons/im";
 const CartItem = ({ cartItem }) => {
   return (
-    <div className="flex flex-col items-start justify-between space-y-2">
-      <h1 className="text-md font-bold uppercase"> {cartItem.name} </h1>
-      <div>
-        
+    <div className="flex flex-col items-start text-black justify-between space-y-4 border w-[400px] p-4">
+      <h1 className="text-xl font-bold capitalize"> {cartItem.name} </h1>
+      <div className="flex flex-row items-center justify-between w-full ">
+        <div className="flex flex-row items-center space-x-2">
+          <span className="font-bold italic">Rs {cartItem.price}</span>
+          <span>
+            <ImCross size={30} />
+          </span>
+          <span>{cartItem.amount} </span>
+        </div>
+        <div className="flex flex-row items-center justify-between space-x-4">
+          <AiFillPlusCircle size={40} />
+          <AiFillMinusCircle size={40} />
+        </div>
       </div>
     </div>
   );
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,15 +1,21 @@
 import React, { useContext } from "react";
 import CartContext from "../store/cart-context";
 import CartItem from "../components/CartItem";
+
 const Cart = () => {
   const cartCtx = useContext(CartContext);
 
   return (
     <div className="m-12 p-12 border ">
-      <div className=" flex flex-col items-start justify-between space-y-4">
-        {cartCtx.items.map((cartItem) => (
-          <CartItem cartItem={cartItem} />
-        ))}
+      <div className="flex flex-row items-start justify-between">
+        <div className=" flex flex-col items-start border justify-between space-y-4">
+          {cartCtx.items.map((cartItem) => (
+            <CartItem cartItem={cartItem} />
+          ))}
+        </div>
+        <div className="border w-[400px] p-12 ">
+          Total Amount : {cartCtx.totalAmount}
+        </div>
       </div>
     </div>
   );
